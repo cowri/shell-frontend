91a4f395ece5faa58d00f3ff3bb0595fec0d9cf5
clinton-jones <clbembryjr@gmail.com>
2020-04-02 11:02:07 -0700

    overview

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import { withTheme } from '@material-ui/core/styles'
 
-const StyledOverview = styled.div`
+const StyledOverview = withTheme(styled.div`
   align-items: center;
+  border-radius: ${props => props.theme.shape.borderRadius}px;
   display: flex;
   flex: 1;
   font-size: 36px;
   justify-content: space-between;
-  margin: 24px 0;
+  margin: 48px;
   padding: 24px 0;
   @media (max-width: 512px) {
-    margin: 12px 0;
+    margin: 24px 0;
     padding: 12px 0;
   }
-`
+`)
 
 const Overview = ({ children }) => (
   <StyledOverview>
