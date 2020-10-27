fbf2d97bcc429280058a04e69671655afea789b8
realisation <realisation@protonmail.com>
2020-10-27 10:52:56 -1000

    thing

--- src/components/ModalAwaitingTx/ModalAwaitingTx.js
+++ src/components/ModalAwaitingTx/ModalAwaitingTx.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import config from "../../mainnet.one.dai.usdc.usdt.susd.config"
+import config from "../../kovan.ctokens.config"
 
 import styled from 'styled-components'
 
