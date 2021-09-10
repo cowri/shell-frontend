d8f04928e26bdd2b6896630409422553ef7089ce
Spiridon Robakidze <spiridonelectro@gmail.com>
2021-09-10 13:59:19 +0200

    clean up

--- src/utils/Farm.js
+++ src/utils/Farm.js
@@ -151,7 +151,6 @@ export class Farm extends NumericFormats {
   }
 
   async claim(onTxHash, onConfirmation, onError) {
-    alert('claim')
     const gasPrice = await this.web3.eth.getGasPrice();
     const gas = await this.managerContract.methods.claim().estimateGas({from: this.account});
     return this.managerContract.methods.claim().send({
