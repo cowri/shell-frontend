0c4714ecded27c74ae5518db8dfa973f29eb3490
Spiridon Robakidze <spiridonelectro@gmail.com>
2021-09-25 11:44:36 +0200

    change xdai cmp exchange link wording

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,6 +21,7 @@ const StyledHeaderLink = styled.a`
   font-size: 20px;
   font-weight: bold;
   margin-bottom: 15px;
+  white-space: nowrap;
   &:hover {
     text-decoration: underline;
   }
@@ -118,15 +119,11 @@ const Header = ({goToIndexTab}) => {
               {!IS_ETH && <StyledHeaderLink href="https://component.finance/" target="_blank">Component on ETH</StyledHeaderLink>}
               {!IS_BSC && <StyledHeaderLink href="https://bsc.component.finance/" target="_blank">Component on BSC</StyledHeaderLink>}
               {!IS_XDAI && <StyledHeaderLink href="https://xdai.component.finance/" target="_blank">Component on xDAI</StyledHeaderLink>}
+              <StyledHeaderLink href="https://app.uniswap.org/#/swap?inputCurrency=0x9f20ed5f919dc1c1695042542c13adcfc100dcab&outputCurrency=ETH" target="_blank">CMP on Uniswap</StyledHeaderLink>
+              <StyledHeaderLink href="https://app.balancer.fi/#/trade/ether/0x9f20ed5f919dc1c1695042542c13adcfc100dcab" target="_blank">CMP on Balancer</StyledHeaderLink>
+              {IS_BSC && <StyledHeaderLink href="https://pancakeswap.finance/swap?inputCurrency=0x96124f7382a0ed672bba8f9b92208434eabcfb40&outputCurrency=BNB" target="_blank">CMP on PancakeSwap</StyledHeaderLink>}
+              {IS_XDAI && <StyledHeaderLink href="https://app.sushi.com/swap?inputCurrency=0x911F196Ed489e41C8B45B5C56FEce021C27a6159&outputCurrency=0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d" target="_blank">CMP on SushiSwap</StyledHeaderLink>}
               <StyledHeaderLink href="https://docs.component.finance/" target="_blank">Docs</StyledHeaderLink>
-              {
-                IS_ETH
-                  ? <StyledHeaderLink href="https://app.uniswap.org/#/swap?inputCurrency=0x9f20ed5f919dc1c1695042542c13adcfc100dcab&outputCurrency=ETH" target="_blank">CMP on Uniswap</StyledHeaderLink>
-                  : IS_BSC
-                  ? <StyledHeaderLink href="https://pancakeswap.finance/swap?inputCurrency=0x96124f7382a0ed672bba8f9b92208434eabcfb40&outputCurrency=BNB" target="_blank">CMP on PancakeSwap</StyledHeaderLink>
-                  : <StyledHeaderLink href="https://app.sushi.com/swap?inputCurrency=0x911F196Ed489e41C8B45B5C56FEce021C27a6159&outputCurrency=0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d" target="_blank">CMP on HoneySwap</StyledHeaderLink>
-              }
-              {IS_ETH && <StyledHeaderLink href="https://app.balancer.fi/#/trade/ether/0x9f20ed5f919dc1c1695042542c13adcfc100dcab" target="_blank">CMP on Balancer</StyledHeaderLink>}
               <Button onClick={() => {loggedIn ? disconnect() : selectWallet()}}>{loggedIn ? 'Disconnect' : 'Connect'}</Button>
             </MobileMenuContainer>
           )}
