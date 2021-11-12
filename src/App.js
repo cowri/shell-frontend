9f350139b14fbbb635d29cbcf396c3136cd7c88b
Spiridon Robakidze <spiridonelectro@gmail.com>
2021-11-12 13:21:26 +0100

    add page title for FTM

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { ThemeProvider } from '@material-ui/core/styles'
 import theme from './theme'
 
 import Dashboard from './views/Dashboard'
-import { IS_BSC, IS_XDAI } from "./constants/chainId";
+import {IS_BSC, IS_FTM, IS_XDAI} from './constants/chainId';
 
 function App() {
 
   useEffect(() => {
-    document.title = `Component on ${IS_BSC ? 'BSC' : IS_XDAI ? 'xDAI' : 'ETH'}`
+    document.title = `Component on ${IS_BSC ? 'BSC' : IS_XDAI ? 'xDAI' : IS_FTM ? 'FTM' : 'ETH'}`
 
     if (+process.env.REACT_APP_CHAIN_ID === 1) {
       document.documentElement.classList.add('fantom')
